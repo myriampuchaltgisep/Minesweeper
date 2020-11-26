b1992636aeefa18f4ccadfa96f7235f068dbaea3
Myriam <myriam.puchalt@ibm.com>
2020-11-26 22:30:48 +0100

    chore: add comment to index.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+// Execute the game on the console
 const Game = require("./game");
 const Board = require('./board');
 const minefield = [['2', '2', '1'], ['*', '*', '2'], ['3', '*', '2']];
-
 
 const board = new Board(minefield);
 const game = new Game(board);
